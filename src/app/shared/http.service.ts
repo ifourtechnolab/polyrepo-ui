1c00d2f20a9f6853a00b520b2ee715e272b3e899
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-03-28 09:42:02 +0530

    feat  : name changes of cards

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,34 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
+  private URL = '';
+  private tokenURL = 'http://192.168.0.181:8080/polyrepo/analyser/auth/';
 
-  private URL = "";
-  private tokenURL="http://192.168.0.181:8080/polyrepo/analyser/auth/";
+  constructor(private http: HttpClient) {}
 
-
-  constructor(private http : HttpClient) { }
-
-  // data(pqr : string){
-  //   let dataItem = pqr;
-  // }
-
-  getData(authToken:any,orgName:any){
-    this.URL = "http://192.168.0.181:8080/polyrepo/analyser/org/"+orgName;
-    return this.http.get(this.URL,{headers:new HttpHeaders({
-      'Authorization':authToken,
-    })
-  });
-}
-
-  public getAuthentication(tokenValue:any)
+  getData(authToken: any, orgName: any) 
   {
-    return this.http.get(this.tokenURL,{headers:new HttpHeaders({
-      'Authorization':tokenValue,
-    })});
+    this.URL = 'http://192.168.0.181:8080/polyrepo/analyser/org/' + orgName;
+    return this.http.get(this.URL, {
+      headers: new HttpHeaders({
+        Authorization: authToken,
+      }),
+    });
   }
 
+  public getAuthentication(tokenValue: any) 
+  {
+    return this.http.get(this.tokenURL, {
+      headers: new HttpHeaders({
+        Authorization: tokenValue,
+      }),
+    });
+  }
+  
 }

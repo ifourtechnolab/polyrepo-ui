3c3ca753d1c485c5d59db24a9f89148d4c60c8af
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-03-29 17:48:55 +0530

    add : add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DemoComponent } from './demo/demo.component';
+import { RepositoryPageComponent } from './repository-page/repository-page.component';
+
+const routes: Routes = [
+  { path: '', component: DemoComponent },
+  { path: 'repository-page',component:RepositoryPageComponent},
+  { path: '**', component: DemoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

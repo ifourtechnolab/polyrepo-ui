ae099b64e31da080fe0cf505006c5247af0c2436
iFour-Urvesh <99787208+iFour-Urvesh@users.noreply.github.com>
2022-03-30 18:14:14 +0530

    Revert "repository UI"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DemoComponent } from './demo/demo.component';
-import { RepositoryPageComponent } from './repository-page/repository-page.component';
-
-const routes: Routes = [
-  { path: '', component: DemoComponent },
-  { path: 'repository-page',component:RepositoryPageComponent},
-  { path: '**', component: DemoComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms'
 import { MatGridListModule } from '@angular/material/grid-list';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
-
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { RepositoryPageComponent } from './repository-page/repository-page.component';
-import { ShowRepositoryComponent } from './repository-page/show-repository/show-repository.component';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
-
+import { CardsComponent } from './cards/cards.component';
+import { BodyComponent } from './body/body.component';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    RepositoryPageComponent,
-    ShowRepositoryComponent,    
+    CardsComponent,
+    BodyComponent,    
   ],
   imports: [
     BrowserModule,
@@ -33,10 +28,7 @@ import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
     MatCardModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
-    HttpClientModule,
-    RouterModule,
-    NgMultiSelectDropDownModule,
-    FormsModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

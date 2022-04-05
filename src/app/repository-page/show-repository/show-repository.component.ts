1e1f941adf09e1ebc4b230b32941bee76f593a41
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-04-05 11:18:21 +0530

    feat : repository dropdown

--- src/app/repository-page/show-repository/show-repository.component.ts
+++ src/app/repository-page/show-repository/show-repository.component.ts
@@ -1,68 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpService } from '../../shared/http.service';
 import * as _ from 'lodash';
+import { MatDialog } from '@angular/material/dialog';
+import { AddrepositoryComponent } from '../addrepository/addrepository.component';
 
 @Component({
   selector: 'app-show-repository',
   templateUrl: './show-repository.component.html',
-  styleUrls: ['./show-repository.component.css']
+  styleUrls: ['./show-repository.component.css'],
 })
 export class ShowRepositoryComponent implements OnInit {
+ 
+  repoName!:string ;
+  
+  constructor(public matDialog: MatDialog) { }
 
-  dropdownList: any = [];
-  selectedItems: any = [];
-  dropdownSettings = {};
-  tokenValue: any;
-  products: any;
-  repoNameList: any;
-  orgLogin: any;
-  authToken: any;
-  selectedI:any = [];
+  ngOnInit(): void { }
 
-  constructor(private http: HttpService) { }
-
-  ngOnInit(): void {
-    this.authToken = localStorage.getItem('token');
-    this.orgLogin = localStorage.getItem('orgLogin');
-    this.dropdownSettings = {
-      singleSelection: false,
-      idField: 'id',
-      textField: 'name',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      itemsShowLimit: 2,
-      allowSearchFilter: false
-    };
-    this.getRecords();
+  openDialog() {
+    this.matDialog.open(AddrepositoryComponent);
+    const openDialog = this.matDialog.open(AddrepositoryComponent);
+    // .afterClosed().subscribe((res:any)=>{
+    //   debugger
+    //   this.repopenDialogoName=res.name;
+    // });
   }
-
-  // get Data from API
-  getRecords() {
-    this.http
-      .getRepoList(this.authToken, this.orgLogin)
-      .subscribe((RepoList: any) => {
-        console.log(RepoList);
-        this.repoNameList = RepoList.edges.map((x: any) => {
-          return {
-            id: x.repository.name,
-            name: x.repository.name
-          }
-         
-
-        });
-      
-      });
-      
-  }
-
-  // selected values
-  onItemSelect(item: any) {
-    this.selectedI+=JSON.stringify(item);
-    console.log(this.selectedI);
-  }
-
-  onSelectAll(items: any) {
-    console.log(items);
-  }
-
 }

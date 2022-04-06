5b44c08a2d829fe6cc434dfca8c3fa4b5361eb24
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-04-06 13:18:06 +0530

    add : component of pr analysis

---
+++ src/app/repository-page/pranalysis/pranalysis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pranalysis',
+  templateUrl: './pranalysis.component.html',
+  styleUrls: ['./pranalysis.component.css']
+})
+export class PranalysisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

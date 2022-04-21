446a783293692c0aa505a067bd9b204760d460cc
Jeel-Bhad <100343944+Jeel-Bhad@users.noreply.github.com>
2022-04-21 12:33:37 +0530

    add: set environment variable

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,15 +12,16 @@ export class HttpService {
   private repoSearchUrl = '';
   private idlePrUrl='';
   private unmergedPrUrl='';
-  private tokenURL = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/auth/';
+  private tokenURL = '';
   private criticalIssue = '';
   private AvgTimeP1 = '';
   private AvgTimeP2 = '';
+  private userUrl=''; 
 
   constructor(private http: HttpClient) { }
 
   getData(authToken: any, orgName: any) {
-    this.URL = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/' + orgName;
+    this.URL = environment.apiUrl +'/org/'+ orgName;
     return this.http.get(this.URL, {
       headers: new HttpHeaders({
         Authorization: authToken,
@@ -29,6 +30,7 @@ export class HttpService {
   }
 
   public getAuthentication(tokenValue: any) {
+    this.tokenURL=environment.apiUrl+'/auth';
     return this.http.get(this.tokenURL, {
       headers: new HttpHeaders({
         Authorization: tokenValue,
@@ -37,7 +39,7 @@ export class HttpService {
   }
 
   public getOrgProfile(authToken: any, orgLogin: any) {
-    this.OrgProfileUrl = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/orgProfile';
+    this.OrgProfileUrl = environment.apiUrl + '/org/' +orgLogin + '/orgProfile';
     return this.http.get(this.OrgProfileUrl, {
 
       headers: new HttpHeaders({
@@ -47,7 +49,7 @@ export class HttpService {
   }
 
   public getRepoList(authToken: any, orgLogin: any) {
-    this.repoListUrl = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/repo';
+    this.repoListUrl = environment.apiUrl +'/org/' + orgLogin + '/repo';
     return this.http.get(this.repoListUrl, {
       headers: new HttpHeaders({
         Authorization: authToken,
@@ -57,7 +59,7 @@ export class HttpService {
 
   //create a url to fetch next page data
   public getNextPageRepoList(authToken: any, nextPageHash: any, orgLogin: any) {
-    this.repoListUrl = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/repo/more';
+    this.repoListUrl = environment.apiUrl +'/org/' + orgLogin + '/repo/more';
     return this.http.get(this.repoListUrl, {
       headers: new HttpHeaders({
         Authorization: authToken,
@@ -68,7 +70,7 @@ export class HttpService {
 
 
   public getRepositoryLisByName(authToken: any, orgLogin: any, repoName: any) {
-    this.repoSearchUrl = 'http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/'+orgLogin+'/repo/'+repoName;
+    this.repoSearchUrl = environment.apiUrl+'/org/' +orgLogin+'/repo/'+repoName;
     return this.http.get(this.repoSearchUrl,{
       headers: new HttpHeaders({
         Authorization: authToken,
@@ -78,7 +80,7 @@ export class HttpService {
   
   // critical issues
   public getcriticalIssue(authToken: any, orgLogin: any, days: any,repoListObject:any){
-    this.criticalIssue = 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/org/'+orgLogin+'/repo/issuesWithPriority1/openSinceBefore/'+days ;
+    this.criticalIssue = environment.apiUrl+'/org/' +orgLogin+'/repo/issuesWithPriority1/openSinceBefore/'+days ;
     const httpOptions = {
       headers: new HttpHeaders({
         'Access-Control-Allow-Origin': '*',
@@ -90,20 +92,20 @@ export class HttpService {
 
   // average resolving time for Priority-1 isuues
   public getAvgTimeP1(authToken: any, orgLogin: any){
-    this.AvgTimeP1 = 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/averageResolvingTimeOfP1Issues';
+    this.AvgTimeP1 = environment.apiUrl + '/org/' + orgLogin + '/averageResolvingTimeOfP1Issues';
     return this.http.get(this.AvgTimeP1, {headers: new HttpHeaders({Authorization: authToken})});
   }
 
     // average resolving time for Priority-2 isuues
   public getAvgTimeP2(authToken: any, orgLogin: any){
-    this.AvgTimeP2 = 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/averageResolvingTimeOfP2Issues';
+    this.AvgTimeP2 = environment.apiUrl + '/org/' + orgLogin + '/averageResolvingTimeOfP2Issues';
     return this.http.get(this.AvgTimeP2, {headers: new HttpHeaders({Authorization: authToken})});
   }
 
   public idlePr(authToken:any,orgLogin:any,days:any,jsonArr:any): Observable<any>
   {
     
-    this.idlePrUrl='http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/'+orgLogin+'/repo/prLastUpdate/'+days;
+    this.idlePrUrl=environment.apiUrl+'/org/' +orgLogin+'/repo/prLastUpdate/'+days;
     const httpOptions = {
       headers: new HttpHeaders({
         'Access-Control-Allow-Origin': '*',
@@ -114,7 +116,7 @@ export class HttpService {
   }
   public unmergedpr(authToken:any,orgLogin:any,days:any,jsonArr:any): Observable<any>
   {    
-    this.unmergedPrUrl='http://192.168.0.181:8080/v.0.1/polyrepo/analyser/org/'+orgLogin+'/repo/prUnMerged/'+days;
+    this.unmergedPrUrl=environment.apiUrl+'/org/' +orgLogin+'/repo/prUnMerged/'+days;
     const httpOptions = {
       headers: new HttpHeaders({
         'Access-Control-Allow-Origin': '*',
@@ -123,6 +125,16 @@ export class HttpService {
     };
     return this.http.post<any>(this.unmergedPrUrl, jsonArr, httpOptions);
   }
-
+  
+  public register(Token:any,email:any,passWord:any): Observable<any>
+  {
+    this.userUrl=environment.apiUrl+'/user/register';
+    let data={
+      "bearerToken":Token,
+      "email":email,
+      "password":passWord,
+    }
+    return this.http.post<any>(this.userUrl, data);
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/'
 };
 
 /*

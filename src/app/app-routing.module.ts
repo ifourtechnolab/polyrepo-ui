95738457e6a5a45a4bcfa6882d1aee8968152f01
Jeel-Bhad <100343944+Jeel-Bhad@users.noreply.github.com>
2022-04-21 12:36:30 +0530

    add: routing for component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DemoComponent } from './demo/demo.component';
 import { RepositoryPageComponent } from './repository-page/repository-page.component';
-
+import {LoginComponent} from './authentication/login/login.component';
+import {RegistrationComponent} from './authentication/registration/registration.component';
 const routes: Routes = [
   { path: '', component: DemoComponent },
   { path: 'repository-page',component:RepositoryPageComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'registration',component:RegistrationComponent},
   { path: '**', component: DemoComponent },
 ];
 

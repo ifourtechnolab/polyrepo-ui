368cec044aa1cbe4470c9f375358a6e8c1c1727a
Jeel-Bhad <100343944+Jeel-Bhad@users.noreply.github.com>
2022-04-21 12:42:04 +0530

    add: login ui

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginFormGroup: FormGroup;
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+}

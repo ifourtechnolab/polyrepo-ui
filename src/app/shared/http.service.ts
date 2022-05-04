93de70c5707ec2d3d5db07bf3b1fce26eee5146a
Jeel-Bhad <100343944+Jeel-Bhad@users.noreply.github.com>
2022-05-04 11:12:47 +0530

    feat : interceptor changes

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -58,6 +58,16 @@ export class HttpService {
     return this.http.get(environment.apiUrl + '/org/' + orgLogin + '/averageResolvingTimeOfP2Issues');
   }
 
+  // getting lables
+  public getlablesservice(orgLogin: any,repoListObject:any ){ 
+    return this.http.post<any>( environment.apiUrl+'/org/' + orgLogin + '/repo/labels', repoListObject);
+  }
+
+  // issuer on lables
+  public getlebelissueservice(orgLogin: any,repoListObject:any, label : any ){
+    return this.http.post<any>(environment.apiUrl+'/org/'+ orgLogin +'/repo/'+ label +'/openIssues', repoListObject);
+  }
+  
   //idel PR since X days
   public idlePr(orgLogin:any,days:any,jsonArr:any): Observable<any>
   { 
@@ -76,33 +86,10 @@ export class HttpService {
     return this.http.post<any>(environment.apiUrl+'/user/register', RegistrationFormGroup);
   }
 
-  // getting lables
-  public getlablesservice( authToken: any, orgLogin: any,repoListObject:any ){
-    debugger
-    this.getlabelsURL = 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/org/' + orgLogin + '/repo/labels';
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*',
-        'Authorization': authToken,
-      })
-    };
-    return this.http.post<any>(this.getlabelsURL, repoListObject , httpOptions);
-  }
   //login functionlity
   public login(LoginFormGroup:any):Observable<any>
   {
     return this.http.post<any>(environment.apiUrl+'/user/login',LoginFormGroup);
   }
 
-  // issuer on lables
-  public getlebelissueservice( authToken: any, orgLogin: any,repoListObject:any, label : any ){
-    this.getlebelissueURL = 'http://192.168.0.182:8080/v.0.1/polyrepo/analyser/org/'+ orgLogin +'/repo/'+ label +'/openIssues';
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*',
-        'Authorization': authToken,
-      })
-    };
-    return this.http.post<any>(this.getlebelissueURL, repoListObject , httpOptions);
-  }
 }

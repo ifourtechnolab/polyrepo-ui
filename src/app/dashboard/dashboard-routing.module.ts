9292572a02f257ef8e9ee5dac1a0965157195e75
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-05-17 11:08:16 +0530

    add : routing

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
+import { PinnedqueryComponent } from './pinnedquery/pinnedquery.component';
+import { SavedqueryComponent } from './savedquery/savedquery.component';
+import { TrendcaptureComponent } from './trendcapture/trendcapture.component';
+
 const routes: Routes = [
-  {
-    path:'', component:DashboardComponent
-  }
+  { path:'', component:DashboardComponent,
+  children:[
+    {path:'',component:PinnedqueryComponent},
+    {path:'pinned',component:PinnedqueryComponent},
+    {path:'saved',component:SavedqueryComponent},
+    {path:'trendcapture',component:TrendcaptureComponent},
+  ],
+}
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { PinnedqueryComponent } from './pinnedquery/pinnedquery.component';
+import { SavedqueryComponent } from './savedquery/savedquery.component';
+import { TrendcaptureComponent } from './trendcapture/trendcapture.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PinnedqueryComponent,
+    SavedqueryComponent,
+    TrendcaptureComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule

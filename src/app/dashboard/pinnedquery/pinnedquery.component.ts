5404bcab24cfd2197f069097ef6041eed24c1899
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-05-17 11:07:14 +0530

    add : pinned query component

---
+++ src/app/dashboard/pinnedquery/pinnedquery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pinnedquery',
+  templateUrl: './pinnedquery.component.html',
+  styleUrls: ['./pinnedquery.component.css']
+})
+export class PinnedqueryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

a3a9857d24ab29f6432635cd22e99ebeb7a7157e
iFour-Urvesh <ifour.urvesh@gmail.com>
2022-05-17 11:07:32 +0530

    add : saved query component

---
+++ src/app/dashboard/savedquery/savedquery.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SavedqueryComponent } from './savedquery.component';
+
+describe('SavedqueryComponent', () => {
+  let component: SavedqueryComponent;
+  let fixture: ComponentFixture<SavedqueryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SavedqueryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SavedqueryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dashboard/savedquery/savedquery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-savedquery',
+  templateUrl: './savedquery.component.html',
+  styleUrls: ['./savedquery.component.css']
+})
+export class SavedqueryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
